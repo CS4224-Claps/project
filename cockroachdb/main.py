655f5cfe37329e5883f2bf3a10f86030cdc7db99
Low Jun Kai, Sean <42912708+seanlowjk@users.noreply.github.com>
2021-09-10 13:43:51 +0800

    Implement New Order Xact for cockroachdb (#5)
    * Implement New Order Xact for cockroachdb
    
    * Fix newline characters
    
    * Fix Syntax Issues and Add validate_command decorator
    
    * Fix Other Stuffs
    
    * Revert connection string changes
    
    * Fix Syntax Issues
    
    * Fix Xacts and Syntax Issues
    
    * Fix Rounding of Total Amount
    
    * Fix Final Issues
    
    Co-authored-by: cs4224d <cs4224d@xcnd35.comp.nus.edu.sg>

--- cockroachdb/main.py
+++ cockroachdb/main.py
@@ -4,8 +4,8 @@ import logging
 import psycopg2
 
 from utils.cli import parse_cmdline
-from xacts.example import run # TODO: To Delete when starting to implement Xacts 
-
+from utils.parser import parse 
+from xacts import new_order
 
 def main():
     opt = parse_cmdline()
@@ -14,7 +14,9 @@ def main():
 
     conn = psycopg2.connect(opt.dsn)
 
-    test_run(conn)
+    # Test 2.1 
+    io_line, data_lines = parse("N,1,1,1,2", ["1,1,1","1,1,1"])
+    new_order.execute(conn, io_line, data_lines)
 
     conn.close()
 
