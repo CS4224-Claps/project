0a9ece7686f58f403a5018de6f3d7cf75d92d40d
Low Jun Kai, Sean <42912708+seanlowjk@users.noreply.github.com>
2021-09-12 00:45:12 +0800

    [CDB] Implement Transaction 2.7 - Top-Balance  (#9)
    Co-authored-by: benclmnt <49342399+benclmnt@users.noreply.github.com>

---
+++ cockroachdb/xacts/top_balance.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timezone
+import logging 
+
+from utils.decorators import validate_command
+
+
+@validate_command("T")
+def execute(conn, io_line, data_lines=[]):    
+    with conn.cursor() as cur:
+        sql = """
+            SELECT concat_ws(' ', c_first, c_middle, c_last) as c_name, c_balance, w_name, d_name
+                FROM Customer
+                INNER JOIN Warehouse on w_id = c_w_id
+                INNER JOIN District on d_w_id = c_w_id and d_id = c_d_id
+                WHERE (c_w_id, c_d_id, c_id) IN (
+                    SELECT c_w_id, c_d_id, c_id 
+                        FROM Customer 
+                        ORDER BY c_balance DESC 
+                        LIMIT 10
+                );
+        """
+
+        cur.execute(sql)
+        customers = cur.fetchall()
+
+        print("Customers: ")
+        for customer in customers: 
+            print(*customer)
+
+    conn.commit()
