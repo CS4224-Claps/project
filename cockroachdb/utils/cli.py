e1d45a03ecd23bff8bfe457b79751fa16121a9c7
benclmnt <49342399+benclmnt@users.noreply.github.com>
2021-09-12 13:43:43 +0800

    [CDB] Implement driver program (#15)
    * Implement driver program: reading xacts from file
    
    * Uncomment related customer

--- cockroachdb/utils/cli.py
+++ cockroachdb/utils/cli.py
@@ -1,35 +1,46 @@
-from argparse import ArgumentParser, FileType, RawTextHelpFormatter, Namespace
-from json import load 
+from argparse import ArgumentParser, FileType, RawTextHelpFormatter
+from json import load
+import sys
 
 
 def get_dsn(config):
     return "postgresql://{}:{}@{}:{}/{}?sslmode=require".format(
-        config["username"], 
-        config["password"], 
-        config["host"], 
-        config["port"], 
-        config["database"])
-
-def parse_cmdline():
-    parser = ArgumentParser(description=__doc__,
-                            formatter_class=RawTextHelpFormatter)
-    parser.add_argument(
-        "config_file",
-        help="""\
-Connection Configuration File 
-
-Refer to: config.json.example for the necessary 
-parameters. 
-"""
+        config["username"],
+        config["password"],
+        config["host"],
+        config["port"],
+        config["database"],
     )
 
-    parser.add_argument("-v", "--verbose",
-                        action="store_true", help="print debug info")
 
+def parse_cmdline():
+    parser = ArgumentParser(description=__doc__, formatter_class=RawTextHelpFormatter)
+    parser.add_argument("-v", "--verbose", action="store_true", help="print debug info")
+    parser.add_argument(
+        "-c",
+        dest="config_file",
+        help="connection config file. defaults to config.json",
+        default="config.json",
+        type=FileType("r"),
+    )
+    parser.add_argument(
+        "-i",
+        dest="infile",
+        help="input file containing xacts",
+        nargs="?",
+        type=FileType("r"),
+        default=sys.stdin,
+    )
+    parser.add_argument(
+        "-o", dest="outdir", help="output directory for logs", default="logs"
+    )
     args = parser.parse_args()
 
     if args.config_file:
-        with open(args.config_file, 'r') as f:
-            args.__dict__.update(dsn=get_dsn(load(f)))
+        args.__dict__.update(dsn=get_dsn(load(args.config_file)))
 
     return args
+
+
+if __name__ == "__main__":
+    parse_cmdline()
