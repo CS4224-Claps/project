e53162100c92b2fa9f9c76059604aace4bf27ae8
Bennett Clement <ben@benclmnt.com>
2021-09-12 10:33:10 +0800

    Implement driver program: reading xacts from file

--- cockroachdb/main.py
+++ cockroachdb/main.py
@@ -4,21 +4,45 @@ import logging
 import psycopg2
 
 from utils.cli import parse_cmdline
-from utils.parser import parse 
-from xacts import new_order, top_balance
+from utils.parser import next_xact
+from xacts import (
+    new_order,
+    payment,
+    delivery,
+    order_status,
+    stock,
+    popular_item,
+    top_balance,
+    # related_customer,
+)
+
+command_to_func = {
+    "N": new_order,
+    "P": payment,
+    "D": delivery,
+    "O": order_status,
+    "S": stock,
+    "I": popular_item,
+    "T": top_balance,
+    # "R": related_customer,
+}
+
 
 def main():
     opt = parse_cmdline()
-
     logging.basicConfig(level=logging.DEBUG if opt.verbose else logging.INFO)
+    logging.debug(f"cli option: {opt}")
 
     conn = psycopg2.connect(opt.dsn)
 
-    """
-    # Test 2.1 
-    io_line, data_lines = parse("N,1,1,1,2", ["1,1,1","1,1,1"])
-    new_order.execute(conn, io_line, data_lines)
-    """
+    with opt.infile as f:
+        while True:
+            xact = next_xact(f)
+            if not xact:
+                break
+
+            xact_type, *args = xact
+            command_to_func[xact_type].execute(conn, *args)
 
     conn.close()
 
--- cockroachdb/utils/parser.py
+++ cockroachdb/utils/parser.py
@@ -1,4 +1,13 @@
-def parse(io_line, data_lines=[]):
-    io_line = io_line.split(",")
-    data_lines = [line.split(",") for line in data_lines]
-    return io_line, data_lines
+def next_xact(f):
+    io_line = f.readline()
+
+    if io_line == "":  # EOF
+        return None
+
+    io_line = io_line.rstrip("\n").split(",")
+
+    if io_line[0] != "N":
+        return io_line[0], io_line
+
+    data_lines = [f.readline().rstrip("\n").split(",") for _ in range(int(io_line[-1]))]
+    return io_line[0], io_line, data_lines
