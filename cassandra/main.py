3bd59184310ef23b743c8a640ddf0dbcaf50bddf
XinPei-ng <elainengxp@gmail.com>
2021-09-15 22:55:46 +0800

    add comment ip addresses

--- cassandra/main.py
+++ cassandra/main.py
@@ -1,5 +1,7 @@
 from cassandra.cluster import Cluster
 
+# according to nodetool status here are the ips I have retrieved
+# 192.168.48.255 ,192.168.51.0, 192.168.51.2, 192.168.51.1, 192.168.48.254
 
 def main():
     cluster = Cluster(['192.168.48.255'])
