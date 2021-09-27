1b288c427c851695cf3c3e601026a3a6df0e0950
XinPei-ng <elainengxp@gmail.com>
2021-09-27 21:52:09 +0800

    test

--- cassandra/main.py
+++ cassandra/main.py
@@ -8,9 +8,10 @@ from cassandra.policies import RoundRobinPolicy, RetryPolicy
 # 192.168.48.255 ,192.168.51.0, 192.168.51.2, 192.168.51.1, 192.168.48.254
 
 def main():
-    retry_policy = RetryPolicy()
+
     cluster_profile = ExecutionProfile(load_balancing_policy=RoundRobinPolicy, consistency_level=ConsistencyLevel.QUORUM)
-    cluster = Cluster(contact_points=['192.168.48.255', '192.168.51.0', '192.168.51.2', '192.168.51.1', '192.168.48.254'], execution_profiles=cluster_profile)
+    cluster = Cluster(['192.168.48.255', '192.168.51.0', '192.168.51.2', '192.168.51.1', '192.168.48.254'])
+    cluster.add_execution_profile("profile", cluster_profile)
     session = cluster.connect()
     r = session.execute("SELECT * FROM system_schema.keyspaces;")
     print(r.current_rows)
