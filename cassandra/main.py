0b482823e261e887503b9cf87757e29f16791775
XinPei-ng <elainengxp@gmail.com>
2021-09-27 21:25:49 +0800

    add execution profile

--- cassandra/main.py
+++ cassandra/main.py
@@ -1,9 +1,17 @@
+from cassandra import ConsistencyLevel
 from cassandra.cluster import Cluster
+from cassandra.cluster import ExecutionProfile
+from cassandra.policies import RoundRobinPolicy, RetryPolicy
+
 
 # according to nodetool status here are the ips I have retrieved
 # 192.168.48.255 ,192.168.51.0, 192.168.51.2, 192.168.51.1, 192.168.48.254
 
 def main():
-    cluster = Cluster(['192.168.48.255'])
+    retry_policy = RetryPolicy()
+    cluster_profile = ExecutionProfile(load_balancing_policy=RoundRobinPolicy, consistency_level=ConsistencyLevel.QUORUM)
+    cluster = Cluster(contact_points=['192.168.48.255', '192.168.51.0', '192.168.51.2', '192.168.51.1', '192.168.48.254'], execution_profiles=cluster_profile)
     session = cluster.connect()
+    session.execute("select * from warehouse");
+
 
