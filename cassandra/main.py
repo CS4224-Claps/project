51f286063366c271649f797aa0961432e8337977
XinPei-ng <elainengxp@gmail.com>
2021-09-30 18:34:13 +0800

    fix execution profile

--- cassandra/main.py
+++ cassandra/main.py
@@ -8,10 +8,9 @@ from cassandra.policies import RoundRobinPolicy, RetryPolicy
 # 192.168.48.255 ,192.168.51.0, 192.168.51.2, 192.168.51.1, 192.168.48.254
 
 def main():
-
-    cluster_profile = ExecutionProfile(load_balancing_policy=RoundRobinPolicy, consistency_level=ConsistencyLevel.QUORUM)
-    cluster = Cluster(['192.168.48.255', '192.168.51.0', '192.168.51.2', '192.168.51.1', '192.168.48.254'])
-    ##cluster.add_execution_profile("profile", cluster_profile)
+    contact_points =['192.168.48.255', '192.168.51.0', '192.168.51.2', '192.168.51.1', '192.168.48.254']
+    cluster_profile = ExecutionProfile(load_balancing_policy=RoundRobinPolicy(), consistency_level=ConsistencyLevel.QUORUM)
+    cluster = Cluster(contact_points, execution_profiles={"profile":cluster_profile})
     session = cluster.connect()
     r = session.execute("SELECT * FROM system_schema.keyspaces;")
     print(r.current_rows)
