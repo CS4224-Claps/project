c5b362b546e7ec1b8de6a306ebabf71aa69ddc80
joel-lim <joellimaojun@hotmail.com>
2021-10-06 10:57:24 +0800

    Add setup statements to populate duplicated fields

--- cassandra/utils/setup.py
+++ cassandra/utils/setup.py
@@ -5,6 +5,7 @@ def setup(session, schema_file):
     statements = read_cql_statements(schema_file)
     for statement in statements:
         session.execute(read_cql_statements(schema_file))
+    add_duplicated_fields(session)
 
 def read_cql_statements(file):
     """
@@ -14,3 +15,21 @@ def read_cql_statements(file):
         text = f.read()
         statements = text.split(";")
         return [statement + ";" for statement in statements if statement.strip()]
+
+# there should be a better way to do it
+def add_duplicated_fields(session):
+    add_wtax_to_district(session)
+    add_iprice_and_iname_to_stock(session)
+
+def add_wtax_to_district(session):
+    rows = session.execute("SELECT W_ID, W_TAX FROM wholesale.Warehouse;")
+    for warehouse in rows:
+        for d_id in range(1, 11):
+            session.execute(f"UPDATE wholesale.District SET W_TAX = {warehouse.w_tax} WHERE D_W_ID = {warehouse.w_id} AND D_ID = {d_id}")
+
+def add_iprice_and_iname_to_stock(session):
+    rows = session.execute("SELECT I_ID, I_PRICE, I_NAME FROM wholesale.Item;")
+    warehouses = session.execute("SELECT W_ID FROM wholesale.Warehouse;")
+    for warehouse in warehouses:
+        for item in rows:
+            session.execute(f"UPDATE wholesale.Stock SET I_PRICE = {item.i_price}, I_NAME = '{item.i_name}' WHERE S_I_ID = {item.i_id} AND S_W_ID = {warehouse.w_id};")
