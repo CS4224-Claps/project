505440a37492b802ad3d8695595b5ccb0e0fb400
XinPei-ng <elainengxp@gmail.com>
2021-10-14 22:02:30 +0800

    add in parsing into main

---
+++ cassandra/utils/transaction.py
@@ -0,0 +1,15 @@
+command_to_func = {
+   ## "N": new_order,
+   ## "P": payment,
+   ## "D": delivery,
+   ## "O": order_status,
+   ## "S": stock,
+   ## "I": popular_item,
+   ## "T": top_balance,
+   ## "R": related_customer
+}
+
+def run_xact(xact_type, session, *args):
+    if xact_type in command_to_func:
+        xact = command_to_func.get(xact_type)(session, *args)
+        return xact
