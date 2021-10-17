1f2708505c34bf33d5f3d2296e5b1be6c5a84d32
Sean Low <sean.low.jk@gmail.com>
2021-10-17 20:13:30 +0800

    Fix Schema

--- cockroachdb/dist_xacts/top_balance.py
+++ cockroachdb/dist_xacts/top_balance.py
@@ -7,7 +7,7 @@ def execute(conn, io_line):
     with conn.cursor() as cur:
         sql = """
             SELECT concat_ws(' ', c_first, c_middle, c_last) as c_name, c_balance, w_name, d_name
-                FROM Customer_Read
+                FROM (Customer_Read NATURAL JOIN Customer_Write)
                 INNER JOIN Warehouse_Read on w_id = c_w_id
                 INNER JOIN District_Read on d_w_id = c_w_id and d_id = c_d_id
                 WHERE (c_w_id, c_d_id, c_id) IN (
