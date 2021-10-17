0e58a874804fd3d4bc26ba4f566b8bb68d0c003a
Sean Low <sean.low.jk@gmail.com>
2021-10-17 19:43:18 +0800

    Update Top Balance Xact

---
+++ cockroachdb/dist_xacts/top_balance.py
@@ -0,0 +1,28 @@
+from utils.decorators import validate_command, log_command
+
+
+@validate_command("T")
+@log_command
+def execute(conn, io_line):    
+    with conn.cursor() as cur:
+        sql = """
+            SELECT concat_ws(' ', c_first, c_middle, c_last) as c_name, c_balance, w_name, d_name
+                FROM Customer_Read
+                INNER JOIN Warehouse_Read on w_id = c_w_id
+                INNER JOIN District_Read on d_w_id = c_w_id and d_id = c_d_id
+                WHERE (c_w_id, c_d_id, c_id) IN (
+                    SELECT c_w_id, c_d_id, c_id 
+                        FROM Customer_Write 
+                        ORDER BY c_balance DESC 
+                        LIMIT 10
+                );
+        """
+
+        cur.execute(sql)
+        customers = cur.fetchall()
+
+        print("Customers: ")
+        for customer in customers: 
+            print(*customer)
+
+    conn.commit()
