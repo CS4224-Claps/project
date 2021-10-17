c8c06e87e58ad2f06a8aab84eea931e4e4e20877
Sean Low <sean.low.jk@gmail.com>
2021-10-17 19:30:45 +0800

    Add Order Status Xact

---
+++ cockroachdb/dist_xacts/order_status.py
@@ -0,0 +1,58 @@
+import logging
+
+from utils.decorators import validate_command, log_command
+
+
+@validate_command("O")
+@log_command
+def execute(conn, io_line):
+    # Retrieve Data from IO Line
+    w_id, d_id, c_id = map(int, io_line[1:])
+
+    with conn.cursor() as cur:
+        get_cust_info = """
+            SELECT concat_ws(' ', C_FIRST, C_MIDDLE, C_LAST) as C_NAME, C_BALANCE
+            FROM Customer_Read
+            WHERE C_W_ID = (%s) AND C_D_ID = (%s) AND C_ID = (%s)
+        """
+        cur.execute(get_cust_info, (w_id, d_id, c_id))
+        c_name, c_balance = cur.fetchone()
+
+        get_last_order = """
+            SELECT O_ID, O_ENTRY_D
+            FROM Order_Read
+            WHERE O_W_ID = (%s) AND O_D_ID = (%s) AND O_C_ID = (%s)
+            ORDER BY O_ENTRY_D DESC
+            LIMIT 1;
+        """
+        cur.execute(get_last_order, (w_id, d_id, c_id))
+        o_id, o_entry_d = cur.fetchone()
+
+        get_carrier_id = """
+            SELECT O_CARRIER_ID 
+                FROM Order_Write 
+                WHERE O_W_ID = %s AND O_D_ID = %s AND O_ID = %s AND O_C_ID = %s
+                LIMIT 1; 
+        """
+        cur.execute(get_carrier_id, (w_id, d_id, o_id, c_id))
+        o_carrier_id, = cur.fetchone()
+
+        get_order_items = """
+            SELECT OL_I_ID, OL_SUPPLY_W_ID, OL_QUANTITY, OL_AMOUNT, OL_DELIVERY_D
+            FROM OrderLine_Read NATURAL JOIN OrderLine_Write 
+            WHERE OL_W_ID = (%s) AND OL_D_ID = (%s) AND OL_O_ID = (%s)
+        """
+        cur.execute(get_order_items, (w_id, d_id, o_id))
+        items = cur.fetchall()
+
+        # Output the following information:
+        print("1. Customer: ", c_name, c_balance)
+        print("2. Last Order: ", o_id, o_entry_d, o_carrier_id)
+        print("3. Items: ")
+
+        for item in items:
+            print(*item)
+
+        logging.debug("order_status: status message: %s", cur.statusmessage)
+
+    conn.commit()
