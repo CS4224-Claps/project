051ad23029318904677fb3a4bfd714bac5c9c668
XinPei-ng <elainengxp@gmail.com>
2021-10-19 00:50:00 +0800

    fix bigs

--- cassandra/xacts/delivery.py
+++ cassandra/xacts/delivery.py
@@ -6,45 +6,57 @@ def execute(session: Session, args):
     w_id, carrier_id = map(int, args[1:])
 
     prepare_OID = session.prepare(
-        "SELECT O_ID, O_CARRIER_ID FROM Orders WHERE O_W_ID = ? AND O_D_ID = ?"
+        "SELECT O_ID, O_CARRIER_ID, O_C_ID FROM Orders WHERE O_W_ID = ? AND O_D_ID = ?"
     )
 
     prepare_update_order = session.prepare(
         "UPDATE wholesale.Orders SET O_CARRIER_ID = ? WHERE O_W_ID = ? AND O_D_ID = ? AND O_ID = ?"
     )
 
-    prepare_update_delivery_date = session.prepare(
-        "UPDATE wholesale.OrderLine SET OL_DELIVERY_D = ? WHERE OL_W_ID = ? AND OL_D_ID = ? AND OL_O_ID = ?"
+    prepare_orderline_data = session.prepare(
+        "SELECT OL_AMOUNT, OL_NUMBER FROM OrderLine WHERE O_W_ID = ? AND OL_D_ID = ? AND OL_O_ID = ?"
     )
 
-    prepare_order_amount = session.prepare(
-        "SELECT OL_AMOUNT FROM OrderLine WHERE O_W_ID = ? AND O_D_ID = ? AND O_ID = ?"
+    prepare_update_delivery_date = session.prepare(
+        "UPDATE wholesale.OrderLine SET OL_DELIVERY_D = ? WHERE OL_W_ID = ? AND OL_D_ID = ? AND OL_O_ID = ? AND "
+        "OL_NUMBER = ? "
+    )
+
+    prepare_cust_data = session.prepare(
+        "SELECT C_BALANCE, C_DELIVERY_CNT FROM wholesale.Customer WHERE C_W_ID = ? AND C_D_ID = ? AND C_ID = ?"
     )
 
     prepare_update_customer = session.prepare(
-        "UPDATE wholesale.Customer SET C_BALANCE = C_BALANCE + ?,  C_DELIVERY_CNT = C_DELIVERY_CNT + 1"
-        " WHERE C_W_ID = ? AND C_D_ID = ? AND C_ID = ?"
+        "UPDATE wholesale.Customer SET C_BALANCE = ?,  C_DELIVERY_CNT = ? WHERE C_W_ID = ? AND C_D_ID = ? AND C_ID = ?"
     )
     try:
         session.add_request_init_listener()
         for district in range(1, 11):
             # Get smallest OID with no carrier and Customer
             future = session.execute_async(prepare_OID.bind(w_id, district), trace=True)
-            oid, cid = future.add_callback(get_smallest_oid)
+            oid, carrier_id, c_id = future.add_callback(get_smallest_oid)
 
             # update carrier
-            session.execute(prepare_update_order.bind(carrier_id, w_id, district, oid))
+            session.execute(prepare_update_order.bind(carrier_id, w_id, district, oid), trace=True)
+
+            # prepare order line data
+            ol_rows = session.execute(prepare_orderline_data.bind(w_id, district, oid), trace=True)
+            c_balance_add = get_ol_amt(ol_rows)
+            c_delivery_add = 1
 
             # update all order lines
             date_time = datetime.now()
-            session.execute(prepare_update_delivery_date.bind(date_time, w_id, district, oid), trace=True)
+            for ol_row in ol_rows:
+                session.execute(prepare_update_delivery_date.bind(date_time, w_id, district, oid, ol_row.OL_NUMBER),
+                                trace=True)
 
-            # select order amount
-            future = session.execute_async(prepare_order_amount.bind(w_id, district, oid), trace=True)
-            ol_total_amt = future.add_callback(get_ol_amt)
+            # prepare customer data
+            c_balance, c_delivery = session.execute(prepare_cust_data.bind(w_id, district, c_id))
+            c_balance += c_balance_add
+            c_delivery += 1
 
             # finally update customerrrrrr
-            session.execute(prepare_update_customer.bind(ol_total_amt, w_id, district, oid), trace=True)
+            session.execute(prepare_update_customer.bind(c_balance, c_delivery, w_id, district, c_id), trace=True)
     except TimeoutError as e:
         print("add try again")
 
