15b9dbd4da4aefacc9d9a062991ee2daf4c90b58
joel-lim <joellimaojun@hotmail.com>
2021-10-19 23:36:57 +0800

    Fix cassandra data loading script

--- cassandra/utils/setup.py
+++ cassandra/utils/setup.py
@@ -9,7 +9,7 @@ LOAD_CUSTOMER = "COPY wholesale.Customer (C_W_ID, C_D_ID, C_ID, C_FIRST, C_MIDDL
 LOAD_ORDER = "COPY wholesale.Orders (O_W_ID, O_D_ID, O_ID, O_C_ID, O_CARRIER_ID, O_OL_CNT, O_ALL_LOCAL, O_ENTRY_D) FROM '{}' WITH NULL = 'null';"
 LOAD_ITEM = "COPY wholesale.Item (I_ID, I_NAME, I_PRICE, I_IM_ID, I_DATA) FROM '{}';"
 LOAD_ORDERLINE = "COPY wholesale.OrderLine (OL_W_ID, OL_D_ID, OL_O_ID, OL_NUMBER, OL_I_ID, OL_DELIVERY_D, OL_AMOUNT, OL_SUPPLY_W_ID, OL_QUANTITY, OL_DIST_INFO, I_NAME) FROM '{}' WITH NULL = 'null';"
-LOAD_STOCK = "COPY wholesale.Stock (S_W_ID, S_I_ID, S_QUANTITY, S_YTD, S_ORDER_CNT, S_REMOTEj_CNT, S_DIST_01, S_DIST_02, S_DIST_03, S_DIST_04, S_DIST_05, S_DIST_06, S_DIST_07, S_DIST_08, S_DIST_09, S_DIST_10, S_DATA, I_PRICE, I_NAME) FROM '{}';"
+LOAD_STOCK = "COPY wholesale.Stock (S_W_ID, S_I_ID, S_QUANTITY, S_YTD, S_ORDER_CNT, S_REMOTE_CNT, S_DIST_01, S_DIST_02, S_DIST_03, S_DIST_04, S_DIST_05, S_DIST_06, S_DIST_07, S_DIST_08, S_DIST_09, S_DIST_10, S_DATA, I_PRICE, I_NAME) FROM '{}';"
 
 TEMP = 'temp'
 WAREHOUSE = 'warehouse.csv'
@@ -107,13 +107,14 @@ def load_data(data_folder):
     Loads data from the data_folder for unmodified csvs, and the TEMP folder for modified csvs.
     This uses the COPY command which is only usable in cqlsh, not via the python driver.
     """
-    subprocess.run(['cqlsh', 'xcnd35'], input=LOAD_WAREHOUSE.format(os.path.join(data_folder, WAREHOUSE)), encoding='utf-8')
-    subprocess.run(['cqlsh', 'xcnd35'], input=LOAD_DISTRICT.format(os.path.join(TEMP, DISTRICT)), encoding='utf-8')
-    subprocess.run(['cqlsh', 'xcnd35'], input=LOAD_CUSTOMER.format(os.path.join(TEMP, CUSTOMER)), encoding='utf-8')
-    subprocess.run(['cqlsh', 'xcnd35'], input=LOAD_ORDER.format(os.path.join(data_folder, ORDER)), encoding='utf-8')
-    subprocess.run(['cqlsh', 'xcnd35'], input=LOAD_ITEM.format(os.path.join(data_folder, ITEM)), encoding='utf-8')
-    subprocess.run(['cqlsh', 'xcnd35'], input=LOAD_ORDERLINE.format(os.path.join(TEMP, ORDERLINE)), encoding='utf-8')
-    subprocess.run(['cqlsh', 'xcnd35'], input=LOAD_STOCK.format(os.path.join(TEMP, STOCK)), encoding='utf-8')
+    cmd = ['cqlsh', 'xcnd35']
+    subprocess.run(cmd, input=LOAD_WAREHOUSE.format(os.path.join(data_folder, WAREHOUSE)), encoding='utf-8')
+    subprocess.run(cmd, input=LOAD_DISTRICT.format(os.path.join(TEMP, DISTRICT)), encoding='utf-8')
+    subprocess.run(cmd, input=LOAD_CUSTOMER.format(os.path.join(TEMP, CUSTOMER)), encoding='utf-8')
+    subprocess.run(cmd, input=LOAD_ORDER.format(os.path.join(data_folder, ORDER)), encoding='utf-8')
+    subprocess.run(cmd, input=LOAD_ITEM.format(os.path.join(data_folder, ITEM)), encoding='utf-8')
+    subprocess.run(cmd, input=LOAD_ORDERLINE.format(os.path.join(TEMP, ORDERLINE)), encoding='utf-8')
+    subprocess.run(cmd, input=LOAD_STOCK.format(os.path.join(TEMP, STOCK)), encoding='utf-8')
 
 def create_temp_folder():
     if not os.path.isdir(TEMP):
