24ddcd678529757e96579cffeea2888f33102244
cs4224d <cs4224d@xcnd35.comp.nus.edu.sg>
2021-10-19 21:09:58 +0800

    fix delivery.py

--- cassandra/xacts/delivery.py
+++ cassandra/xacts/delivery.py
@@ -6,7 +6,7 @@ def execute(session: Session, args):
     w_id, carrier_id = map(int, args[1:])
 
     prepare_OID = session.prepare(
-        "SELECT O_ID, O_CARRIER_ID, O_C_ID FROM Orders WHERE O_W_ID = ? AND O_D_ID = ?"
+        "SELECT O_ID, O_CARRIER_ID, O_C_ID FROM wholesale.Orders WHERE O_W_ID = ? AND O_D_ID = ?"
     )
 
     prepare_update_order = session.prepare(
@@ -18,7 +18,7 @@ def execute(session: Session, args):
     )
 
     prepare_orderline_data = session.prepare(
-        "SELECT OL_AMOUNT, OL_NUMBER FROM OrderLine WHERE O_W_ID = ? AND OL_D_ID = ? AND OL_O_ID = ?"
+        "SELECT OL_AMOUNT, OL_NUMBER FROM wholesale.OrderLine WHERE OL_W_ID = ? AND OL_D_ID = ? AND OL_O_ID = ?"
     )
 
     prepare_update_delivery_date = session.prepare(
@@ -38,43 +38,46 @@ def execute(session: Session, args):
         #session.add_request_init_listener()
         for district in range(1, 11):
             # Get smallest OID with no carrier and Customer
-            future = session.execute_async(prepare_OID.bind(w_id, district), trace=True)
-            oid, carrier_id, c_id = future.add_callback(get_smallest_oid)
-
+            oid_rows = session.execute(prepare_OID.bind((w_id, district)), trace=True)
+            print(district)
+            print(oid_rows)        
+            oid,  c_id = get_smallest_oid(oid_rows)
+    
             # update carrier
-            session.execute(prepare_update_order.bind(carrier_id, w_id, district, oid), trace=True)
+            session.execute(prepare_update_order.bind((carrier_id, w_id, district, oid)), trace=True)
 
             # prepare order line data
-            ol_rows = session.execute(prepare_orderline_data.bind(w_id, district, oid), trace=True)
+            ol_rows = session.execute(prepare_orderline_data.bind((w_id, district, oid)), trace=True)
             c_balance_add = get_ol_amt(ol_rows)
             c_delivery_add = 1
 
             # update all order lines
             date_time = datetime.now()
             for ol_row in ol_rows:
-                session.execute(prepare_update_delivery_date.bind(date_time, w_id, district, oid, ol_row.OL_NUMBER),
+                session.execute(prepare_update_delivery_date.bind((date_time, w_id, district, oid, ol_row.OL_NUMBER)),
                                 trace=True)
 
             # prepare customer data
-            c_balance, c_delivery = session.execute(prepare_cust_data.bind(w_id, district, c_id))
-            c_balance += c_balance_add
-            c_delivery += 1
+            c_row = session.execute(prepare_cust_data.bind((w_id, district, c_id))).one()
+            print(c_row)
+            c_balance = c_row.c_balance + c_balance_add
+            c_delivery = c_row.c_delivery_cnt + 1
 
             # finally update customerrrrrr
-            session.execute(prepare_update_customer.bind(c_balance, c_delivery, w_id, district, c_id), trace=True)
+            session.execute(prepare_update_customer.bind((c_balance, c_delivery,w_id, district, c_id)), trace=True)
     except TimeoutError as e:
         print("add try again")
 
 
 def get_smallest_oid(rows):
     for row in rows:
-        if not row.O_CARRIER.ID:
-            return int(row.O_ID), int(row.O_C_ID)
+        if not row.o_carrier_id:
+            return int(row.o_id), int(row.o_c_id)
 
 
 def get_ol_amt(rows):
     total = 0
     for row in rows:
-        total += row.OL_AMOUNT
+        total += row.ol_amount
 
     return total
