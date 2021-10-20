d0a74b5b588c68bcc11967c2c521dbb8269908ad
joel-lim <joellimaojun@hotmail.com>
2021-10-20 18:47:24 +0800

    Add keyspace prefix for popular item queries

--- cassandra/xacts/popular_item.py
+++ cassandra/xacts/popular_item.py
@@ -3,7 +3,7 @@ def execute(session, input_arr, data_lines=[]):
 
     # Get D_NEXT_O_ID
     prepare_district = session.prepare(
-        "SELECT D_NEXT_O_ID FROM District WHERE D_W_ID = ? AND D_ID = ?")
+        "SELECT D_NEXT_O_ID FROM wholesale.District WHERE D_W_ID = ? AND D_ID = ?")
     rows = session.execute(prepare_district.bind((w_id, d_id)))
     district = rows[0]
     d_next_o_id = int(district.d_next_o_id)
@@ -12,7 +12,7 @@ def execute(session, input_arr, data_lines=[]):
     orders = []
     for o_id in range(d_next_o_id - num_orders, d_next_o_id):
         prepare_order = session.prepare(
-            "SELECT O_ID, O_ENTRY_D, O_C_ID FROM Orders WHERE O_W_ID = ? AND O_D_ID = ? AND  O_ID = ?")
+            "SELECT O_ID, O_ENTRY_D, O_C_ID FROM wholesale.Orders WHERE O_W_ID = ? AND O_D_ID = ? AND  O_ID = ?")
         rows = session.execute(prepare_order.bind((w_id, d_id, o_id)))
         order = rows[0]
         orders.append(order)
@@ -21,7 +21,7 @@ def execute(session, input_arr, data_lines=[]):
     customers = []
     for order in orders:
         prepare_customer = session.prepare(
-            "SELECT C_FIRST, C_MIDDLE, C_LAST FROM Customer WHERE C_W_ID = ? AND C_D_ID = ? AND C_ID = ?")
+            "SELECT C_FIRST, C_MIDDLE, C_LAST FROM wholesale.Customer WHERE C_W_ID = ? AND C_D_ID = ? AND C_ID = ?")
         rows = session.execute(prepare_customer.bind((w_id, d_id, order.o_c_id)))
         customer = rows[0]
         customers.append(customer)
@@ -35,7 +35,7 @@ def execute(session, input_arr, data_lines=[]):
     popular_item_id_names = {}
     for order in orders:
         prepare_orderline = session.prepare(
-            "SELECT OL_I_ID, I_NAME, OL_QUANTITY FROM OrderLine WHERE OL_W_ID = ? AND OL_D_ID = ? AND OL_O_ID = ?")
+            "SELECT OL_I_ID, I_NAME, OL_QUANTITY FROM wholesale.OrderLine WHERE OL_W_ID = ? AND OL_D_ID = ? AND OL_O_ID = ?")
         rows = session.execute(prepare_orderline.bind((w_id, d_id, order.o_id)))
         order_item_set = set()
         max_qty = 0
