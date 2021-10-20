c1999272317d071c8a37368b645f78552601a12b
Sean Low <sean.low.jk@gmail.com>
2021-10-20 21:56:37 +0800

    Test Run

--- performance/utils/cassandra.py
+++ performance/utils/cassandra.py
@@ -48,15 +48,12 @@ def get_cassandra_stats(session):
     d_ytd, d_next_o_id = session.execute(district_stmt)[0]
     c_balance, c_ytd_payment, c_payment_cnt, c_delivery_cnt = session.execute(customer_stmt)[0]
     o_id, o_ol_cnt = session.execute(order_stmt)[0]
-    ol_amount, ol_quantity = session.execute(orderline_stmt)[0]
+    # ol_amount, ol_quantity = session.execute(orderline_stmt)[0]
     s_quantity, s_ytd, s_order_cnt, s_remote_cnt = session.execute(stock_stmt)[0]
 
     final_state = [
         w_ytd, d_ytd, d_next_o_id, c_balance, c_ytd_payment, c_payment_cnt, c_delivery_cnt, 
         o_id, o_ol_cnt, ol_amount, ol_quantity, s_quantity, s_ytd, s_order_cnt, s_remote_cnt
     ]
-
-    if len(final_state) != 15:
-        raise ValueError("Final statistics missing values")
 
     return final_state
