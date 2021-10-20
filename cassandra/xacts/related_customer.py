ae53a7bf1bcfa28181fd10d2194da87f80d232c8
joel-lim <joellimaojun@hotmail.com>
2021-10-20 18:45:00 +0800

    Add keyspace prefix to related customer queries

--- cassandra/xacts/related_customer.py
+++ cassandra/xacts/related_customer.py
@@ -3,14 +3,14 @@ def execute(session, input_arr, data_lines=[]):
 
     # Get o_ids of all the customer's orders
     prepare_orders = session.prepare(
-        "SELECT O_ID FROM Orders WHERE O_W_ID = ? AND O_D_ID = ? AND O_C_ID = ? ALLOW FILTERING")
+        "SELECT O_ID FROM wholesale.Orders WHERE O_W_ID = ? AND O_D_ID = ? AND O_C_ID = ? ALLOW FILTERING")
     rows = session.execute(prepare_orders.bind((c_w_id, c_d_id, c_id)))
     o_ids = [order.o_id for order in rows]
 
     # Get items for each order
     order_itemsets = []
     prepare_orderline = session.prepare(
-        "SELECT OL_I_ID FROM Orderline WHERE OL_W_ID = ? AND OL_D_ID = ? AND OL_O_ID = ?")
+        "SELECT OL_I_ID FROM wholesale.Orderline WHERE OL_W_ID = ? AND OL_D_ID = ? AND OL_O_ID = ?")
     for o_id in o_ids:
         rows = session.execute(prepare_orderline.bind((c_w_id, c_d_id, o_id)))
         order_itemsets.append({orderline.ol_i_id for orderline in rows})
@@ -18,7 +18,7 @@ def execute(session, input_arr, data_lines=[]):
     # For each of customer's orders, find all orders from different warehouse with >=2 common items
     related_orders = set()
     prepare_orderline = session.prepare(
-        "SELECT * FROM Orderline_by_iid WHERE OL_I_ID IN ?")
+        "SELECT * FROM wholesale.Orderline_by_iid WHERE OL_I_ID IN ?")
     for order_itemset in order_itemsets:
         seen_order_identifiers = set()
         rows = session.execute(prepare_orderline.bind((order_itemset,)))
@@ -35,7 +35,7 @@ def execute(session, input_arr, data_lines=[]):
 
     # Retrieve c_id for these related customers' orders
     prepare_orders = session.prepare(
-        "SELECT O_C_ID FROM Orders WHERE O_W_ID = ? AND O_D_ID = ? AND O_ID = ?")
+        "SELECT O_C_ID FROM wholesale.Orders WHERE O_W_ID = ? AND O_D_ID = ? AND O_ID = ?")
     related_customers = []
     for w_id, d_id, o_id in related_orders:
         rows = session.execute(prepare_orders.bind((w_id, d_id, o_id)))
