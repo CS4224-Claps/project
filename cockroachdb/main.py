05e87e76faadfffabb961dd5b67f56789b4810dd
Bennett Clement <ben@benclmnt.com>
2021-10-21 16:49:40 +0800

    Crude retry handling

--- cockroachdb/main.py
+++ cockroachdb/main.py
@@ -1,5 +1,6 @@
 import logging, logging.handlers
 import psycopg2
+from pathlib import Path
 
 from utils.cli import parse_cmdline
 from utils.connection import TimeLoggingConnection
@@ -29,6 +30,22 @@ command_to_func = {
 }
 
 
+def _run_one_xact(xact, conn, opt, fd):
+    xact_type, *args = xact
+    op = lambda conn: command_to_func[xact_type].execute(conn, *args)
+    try:
+        run_transaction(conn, op, opt.max_retries)
+    except:
+        # if there is any error, we write it to the file noted by fd and move on to the next xact
+
+        # args[0] is the io line
+        fd.write(",".join(args[0]) + "\n")
+        if len(args) > 1:
+            # args[1] is the data line
+            for data_line in args[1]:
+                fd.write(",".join(data_line) + "\n")
+
+
 def main():
     opt = parse_cmdline()
     logging.basicConfig(level=logging.DEBUG if opt.verbose else logging.INFO)
@@ -37,15 +54,42 @@ def main():
     conn = psycopg2.connect(dsn=opt.dsn, connection_factory=TimeLoggingConnection)
     conn.initialize(init_logger(opt.infile, opt.outdir))
 
-    with opt.infile as f:
+    retry_file = f"{opt.outdir}/retry_{Path(opt.infile.name).stem}.txt"
+
+    # do the original transaction from seed file
+    with opt.infile as f, open(retry_file, "a") as rf:
         while True:
-            xact = next_xact(f)
+            xact = next_xact(f.readline(), f)
             if not xact:
                 break
 
-            xact_type, *args = xact
-            op = lambda conn: command_to_func[xact_type].execute(conn, *args)
-            run_transaction(conn, op, opt.max_retries)
+            _run_one_xact(xact, conn, opt, rf)
+
+    # do retries if there is any
+    rf1 = open(retry_file, "r")
+    rf2 = open(retry_file, "a")
+    cur_start, cur_end = 0, rf2.tell()
+
+    with rf1, rf2:
+        while (
+            rf2.tell() > 0 and rf2.tell() > rf1.tell()
+        ):  # there is something written in the previous iteration
+            rf1.seek(cur_start)
+            rf2.write("--------\n")
+            while True:
+                line = rf1.readline()
+                if line.startswith("-"):
+                    continue
+
+                xact = next_xact(line, rf1)
+                if not xact:
+                    break
+
+                _run_one_xact(xact, conn, opt, rf2)
+
+            cur_start, cur_end = cur_end, rf2.tell()
+            rf1.seek(cur_start)
+            rf1.readline()  # we skip the marker -----
 
     conn.close()
 
