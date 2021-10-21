306d2761cc9a174d28ea627be5fb8f9d8546820e
Sean Low <sean.low.jk@gmail.com>
2021-10-21 18:54:01 +0800

    Fix Exec Profile

--- performance/utils/cli.py
+++ performance/utils/cli.py
@@ -1,6 +1,8 @@
 from cassandra import ConsistencyLevel
 from cassandra.cluster import ExecutionProfile, Cluster
-from cassandra.policies import RoundRobinPolicy
+from cassandra.policies import RoundRobinPolicy, DowngradingConsistencyRetryPolicy
+from cassandra.query import tuple_factory
+
 
 from argparse import ArgumentParser, FileType, RawTextHelpFormatter
 from json import load
@@ -24,8 +26,11 @@ def get_cockroach_dsn(conf):
 def get_cassandra_session():
     contact_points = ['192.168.48.255', '192.168.51.0', '192.168.51.2', '192.168.51.1', '192.168.48.254']
     cluster_profile = ExecutionProfile(load_balancing_policy=RoundRobinPolicy(), 
-        consistency_level=ConsistencyLevel.QUORUM,
-        request_timeout=60000)
+        retry_policy=DowngradingConsistencyRetryPolicy(),
+        consistency_level=ConsistencyLevel.LOCAL_QUORUM,
+        serial_consistency_level=ConsistencyLevel.LOCAL_SERIAL,
+        request_timeout=10000,
+        row_factory=tuple_factory)
     cluster = Cluster(contact_points, execution_profiles={"profile": cluster_profile})
     session = cluster.connect()
 
