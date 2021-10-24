2cf7b2ccf36b733d01d895e301cd057a08598feb
Sean Low <sean.low.jk@gmail.com>
2021-10-24 21:45:45 +0800

    Fix Logging

--- cassandra/xacts/order_status.py
+++ cassandra/xacts/order_status.py
@@ -38,8 +38,7 @@ def execute(session: Session, args):
     items = result.result()
     tracing = result.get_query_trace()
 
-    for e in tracing.events:
-        logging.debug(e.datetime, e.duration)
+    logging.debug(e.parameters, e.duration)
 
     for item in items:
         print(item.ol_i_id, item.ol_supply_w_id,
