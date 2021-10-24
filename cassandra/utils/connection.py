108d109e02ca8657c49f427b57f11d71b4d731d1
Sean Low <sean.low.jk@gmail.com>
2021-10-24 17:00:43 +0800

    Modify Exec Profile Default

--- cassandra/utils/connection.py
+++ cassandra/utils/connection.py
@@ -7,9 +7,12 @@ from utils.custom_retry import CustomRetry
 def connection():
 
     contact_points = ['192.168.48.255', '192.168.51.0', '192.168.51.2', '192.168.51.1', '192.168.48.254']
-    cluster_profile = ExecutionProfile(load_balancing_policy=RoundRobinPolicy(),
-                                       consistency_level=ConsistencyLevel.QUORUM,
-                                       retry_policy= CustomRetry(5,2,2))
+    cluster_profile = ExecutionProfile(load_balancing_policy=RoundRobinPolicy(), 
+        retry_policy=CustomRetry(5,2,2),
+        consistency_level=ConsistencyLevel.LOCAL_QUORUM,
+        serial_consistency_level=ConsistencyLevel.LOCAL_SERIAL,
+        request_timeout=10000,
+        row_factory=tuple_factory)
     cluster = Cluster(contact_points, execution_profiles={EXEC_PROFILE_DEFAULT: cluster_profile})
     session = cluster.connect()
 
