5ef1eb351a3fdfc3ee7bea817d799ba0f94e2da3
joel-lim <joellimaojun@hotmail.com>
2021-10-27 19:19:47 +0800

    Optimise top balance slightly

--- cassandra/xacts/top_balance.py
+++ cassandra/xacts/top_balance.py
@@ -1,3 +1,4 @@
+import heapq
 from utils.decorators import log_command
 
 
@@ -11,12 +12,12 @@ def execute(session, args):
         )
     ]
 
-    # Sort customers across all partitions to get top 10 balances globally
-    customers.sort(key=lambda customer: customer.c_balance, reverse=True)
+    # Get top 10 customers, globally
+    top_customers = heapq.nlargest(10, customers, key=lambda customer: customer.c_balance)
 
     # Print info
     print("Customers:")
-    for customer in customers[:10]:
+    for customer in top_customers:
         print(
             f"{customer.c_first} {customer.c_middle} {customer.c_last} {customer.c_balance} {customer.w_name} {customer.d_name}"
         )
