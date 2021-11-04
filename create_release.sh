99842dfbcdc3f83e89a00e1c1aa2e8158b7e0064
Bennett Clement <ben@benclmnt.com>
2021-11-04 14:20:35 +0800

    Create release script

---
+++ create_release.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+roll() {
+    db=$1;
+    target_dir="release/$db"
+    mkdir -p $target_dir;
+    cp -a $db performance $target_dir
+    cp -a README.md run.sh requirements.txt config.json.example run_clients.py $target_dir
+    zip -r release/d.$db.zip $target_dir
+}
+
+rm -rf release/*
+roll cassandra
+roll cockroachdb
+
+
